9750d50e72651fae8d9b69a86a33efa295cd7abc
github username <github email address>
2020-07-13 22:36:12 -0500

    edit links

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
 export default function NavBar() {
 	return (
-		<div>
+		<BrowserRouter>
 			<Link className="item" to="/">
 				About Me
 			</Link>
 			<Link className="item" to="/projects">
 				Projects
 			</Link>
-		</div>
+		</BrowserRouter>
 	);
 }
